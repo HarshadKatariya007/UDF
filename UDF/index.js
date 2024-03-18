7cb1c76922ee1b7e49eaf61a56a538ea84c373bc
Harshad Katariya <159020392+HarshadKatariya007@users.noreply.github.com>
2024-03-18 14:22:00 +0530

    Add files via upload

---
+++ UDF/index.js
@@ -0,0 +1,21 @@
+function setFirstName() 
+{
+    document.querySelector("#Firstname").innerHTML="Harshad";
+    
+}
+
+function setLastName() 
+{
+    document.querySelector("#Lastname").innerHTML="Katariya";
+}
+
+function setFullName()
+{
+    document.querySelector("#Fullname").innerHTML="Harshad Katariya";
+}
+function resetDefaults() 
+{
+    document.querySelector("#Firstname").innerHTML="";
+    document.querySelector("#Lastname").innerHTML="";
+    document.querySelector("#Fullname").innerHTML="";
+}
